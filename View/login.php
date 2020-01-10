96698cc8a1aab5061c53fc4f17c9b307dd9d41b0
kristel van mileghem <kristelvanmileghem@hotmail.com>
2020-01-10 09:24:15 +0100

    age gate login pagina nog niet af

--- View/login.php
+++ View/login.php
@@ -11,7 +11,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <!-- Latest compiled and minified CSS -->
-      <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="../html-css-js/login.css">
     <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
