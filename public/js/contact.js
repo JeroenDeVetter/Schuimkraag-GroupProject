ee946841f7deb04aae86fb2d31a902eb2a51c38a
danny <danny.eeraerts@proximus.be>
2020-01-15 16:01:14 +0100

    Add javascript to contact

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,42 +1,127 @@
 let emailContact = document.querySelector('#email_contact');
 let naamContact = document.querySelector('#naam');
+let onderwerpContact = document.querySelector('#subject_contact');
 let messageContact = document.querySelector('#message');
 
-let foutboodschapLogin = document.querySelector('#error_message_login');
+let foutboodschapContact = document.querySelector('#error_message_contact');
 
 function emailVerifyContact() {
-
-  if (emailInputLogin.value !== "") {
-      if (regmailCheck(emailInputLogin.value)) {
-      emptyMessage(foutboodschapLogin);
-      emailInputLogin.value = cleanemail(emailinput.value);
+  console.log("checkmail");
+  
+  if (emailContact.value !== "") {
+      if (regmailCheck(emailContact.value)) {
+        console.log("check");
+      emptyMessage(foutboodschapContact);
+      emailContact.value = cleanemail(emailContact.value);
       } else {
-      foutboodschapLogin.innerHTML = "<div>Email heeft verkeerd formaat&nbsp;</div><div>&#x274C</div>";
-      toggleErrorMessage(foutboodschapLogin);
+      foutboodschapContact.innerHTML = "<div>Email heeft ongeldig formaat&nbsp;</div><div>&#x274C</div>";
+      toggleErrorMessage(foutboodschapContact);
       }
   }
   else {
-      foutboodschapLogin.innerHTML = "<div>Email is vereist&nbsp;</div><div>&#x274C;</div>";
-      toggleErrorMessage(foutboodschapLogin);
+      foutboodschapContact.innerHTML = "<div>Email is vereist&nbsp;</div><div>&#x274C;</div>";
+      toggleErrorMessage(foutboodschapContact);
   }
 
 }
 
 function naamVerifyContact() {
-  if (this.value !== "") {
+  console.log("naam");
+  console.log(foutboodschapContact.innerHTML);
+  if (naamContact.value !== "") {
+      console.log(foutboodschapContact.innerHTML);
       if (regnameCheck(this.value)) {
           naamContact.value = cleanName(naamContact.value);
-          emptyMessage(foutboodschap);
+          emptyMessage(foutboodschapContact);
       } else {
-          foutboodschap.innerHTML = "<div>Achternaam is niet correct. Min 2 letters, geen getallen of symbolen &nbsp;</div><div>&#x274C</div>";
-          toggleErrorMessage(foutboodschap);
+        foutboodschapContact.innerHTML = "<div>Naam is niet correct. Min 2 letters, geen getallen of symbolen &nbsp;</div><div>&#x274C</div>";
+          toggleErrorMessage(foutboodschapContact);
       }
   } else {
-      foutboodschap.innerHTML = "<div>Achternaam is vereist &nbsp;</div><div>&#x274C;</div>";
-      toggleErrorMessage(foutboodschap);
+    foutboodschapContact.innerHTML = "<div>Naam is vereist &nbsp;</div><div>&#x274C;</div>";
+      toggleErrorMessage(foutboodschapContact);
   }
 }
 
+
+function subjectVerifyContact(){
+  console.log("checkonderwerpe");
+  if (onderwerpContact.value !== "") {
+    if (onderwerpContact.value.length >=2){
+      emptyMessage(foutboodschapContact);
+    } else {
+      foutboodschapContact.innerHTML = "<div>Onderwerp dient minstens 2 karakters te bevatten. &nbsp;</div><div>&#x274C</div>";
+      toggleErrorMessage(foutboodschapContact);
+    }
+  }
+  else {
+    foutboodschapContact.innerHTML = "<div>Onderwerp is vereist. &nbsp;</div><div>&#x274C</div>";
+    toggleErrorMessage(foutboodschapContact);
+  }
+}
+
+function messageVerifyContact(){
+  console.log("message");
+  if (messageContact.value !== "") {
+    if (messageContact.value.length >=2){
+      emptyMessage(foutboodschapContact);
+    } else {
+      foutboodschapContact.innerHTML = "<div>Boodschap dient minstens 2 karakters te bevatten. &nbsp;</div><div>&#x274C</div>";
+      toggleErrorMessage(foutboodschapContact);
+    }
+  }
+  else {
+    foutboodschapContact.innerHTML = "<div>Boodschap is vereist. &nbsp;</div><div>&#x274C</div>";
+    toggleErrorMessage(foutboodschapContact);
+  }
+}
+
+function cleanName(string) {
+  string = string.trim();
+  string = string.replace(/\s+/g, ' ');
+  let newSentence = "";
+  let x = string.split(" ").length;
+  var words = string.split(" ");
+  var firstSubPart;
+  if (string.valueOf() === string.valueOf().toUpperCase()) {
+      for (let i = 0; i < x; i++) {
+          if ((words[i].split("-")).length > 1) {
+              let subWords = words[i].split("-");
+              let newSubSentence2 = "";
+              let k = subWords.length;
+              for (j = 0; j < (k - 1); j++) {
+                  let counter = subWords[j].length;
+                  if (j === 0) {
+                      let firstSubPart1 = subWords[j].substring(0, 1).toUpperCase();
+                      let firstSubPart2 = (subWords[j].substring(1)).toLowerCase();
+                      firstSubPart = firstSubPart1 + firstSubPart2;
+                  } else {
+                      firstSubPart = "";
+                  }
+                  let lastSubPart1 = (subWords[j + 1].substring(0, 1)).toUpperCase();
+                  let lastSubPart2 = (subWords[j + 1].substring(1)).toLowerCase();
+                  let newSubWord = firstSubPart + "-" + lastSubPart1 + lastSubPart2;
+                  newSubSentence2 = newSubSentence2 + newSubWord;
+              }
+              newSentence = newSentence + newSubSentence2 + " ";
+              string = newSentence;
+          } else {
+              var firstPart = words[i].substring(0, 1);
+              var lastPart = words[i].substring(1);
+              firstPart = (words[i].substring(0, 1)).toUpperCase();
+              lastPart = (words[i].substring(1)).toLowerCase();
+              newWord = firstPart + lastPart;
+              newSentence = newSentence + newWord + " ";
+              string = newSentence;
+          }
+      }
+  }
+  return string;
+}
+
+function cleanemail(string) {
+  return string.toLowerCase();
+}
 
 function regmailCheck(mailCheck) {
 
@@ -51,18 +136,18 @@ function regnameCheck(nameCheck) {
   return (nameRegex.test(nameCheck));
 }
 
-function emptyMessage(foutboodschapLogin) {
+function emptyMessage(foutboodschapContact) {
 
-  foutboodschapLogin.innerHTML = "";
-  foutboodschapLogin.classList.remove("show");
-  foutboodschapLogin.classList.add('hide');
+  foutboodschapContact.innerHTML = "";
+  foutboodschapContact.classList.remove("show");
+  foutboodschapContact.classList.add('hide');
 
 }
 
-function toggleErrorMessage(foutboodschapLogin) {
+function toggleErrorMessage(foutboodschapContact) {
 
-  foutboodschapLogin.classList.remove("hide");
-  foutboodschapLogin.classList.add("show");
+  foutboodschapContact.classList.remove("hide");
+  foutboodschapContact.classList.add("show");
 
 }
 
@@ -75,19 +160,35 @@ function removeErrorMessage() {
 
 naamContact.addEventListener('blur', naamVerifyContact)
 emailContact.addEventListener('blur', emailVerifyContact);
+onderwerpContact.addEventListener('blur', subjectVerifyContact);
 messageContact.addEventListener('blur', messageVerifyContact);
 
-foutboodschapLogin.addEventListener("click", removeErrorMessage);
+foutboodschapContact.addEventListener("click", removeErrorMessage);
 
-form.addEventListener('submit', CheckAllLogin);
+form.addEventListener('submit', CheckAllContact);
 
-function CheckAllLogin(event){
-  
-  naamVerifyContact();
+function CheckAllContact(event){
+  let teller = 0;
+  messageVerifyContact();
+  if (foutboodschapContact.innerHTML !== ""){
+    teller +=1
+  }
+  subjectVerifyContact();
+  if (foutboodschapContact.innerHTML !== ""){
+    teller +=1
+  }
   emailVerifyContact();
-  console.log(foutboodschapLogin.innerHTML)
-  if (foutboodschapLogin.innerHTML !== "") {
-    
+  if (foutboodschapContact.innerHTML !== ""){
+    teller +=1
+  }
+  naamVerifyContact();
+  if (foutboodschapContact.innerHTML !== ""){
+    teller +=1
+  }
+  
+  if (teller>0 || (foutboodschapContact.innerHTML !=="")) {
+    console.log(teller);
+    foutboodschapContact.innerHTML = "<div>De input-waarden zijn niet correct &nbsp;</div><div>&#x274C</div>";
     event.preventDefault();
   }
 
